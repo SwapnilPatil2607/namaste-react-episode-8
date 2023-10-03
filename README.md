9743701283b86c637fbd00698c2d793bc9060b89
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2023-10-03 22:02:52 +0530

    init: episode 8

---
+++ README.md
@@ -0,0 +1,5 @@
+# Namaste React episode 8
+
+# key points
+
+1. class based component
---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <h2>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nemo incidunt fugiat tempore, officiis exercitationem praesentium
+        numquam mollitia aliquid reprehenderit nihil dolorem pariatur culpa.
+        Quibusdam omnis recusandae architecto ad dicta.
+      </h2>
+    </div>
+  );
+};
